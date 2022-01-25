4a58177fe0f7c2ffdaa0bc43273a4a50572329a6
shubham changan <98081243+shubhamchangan@users.noreply.github.com>
2022-01-25 17:54:42 +0530

    Add files via upload

---
+++ amazon_shipment.py
@@ -0,0 +1,11 @@
+# If we are purchasing products from Amazon, condition is
+# if ur item price is >= 500 free shipment is available
+# else u have to pay Rs.60 for delivery.
+
+cart = [1999, 200, 500, 399, 780, 899, 499]
+for i in cart:
+    if i >= 500:
+        print('Dear customer free shipment is available')
+    else:
+        print('You have to pay Rs.60 for shipment')
+        
