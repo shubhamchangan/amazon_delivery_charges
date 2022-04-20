a0951820cdcd558735508fc38e14c6e43209d08b
shubham changan <98081243+shubhamchangan@users.noreply.github.com>
2022-04-20 09:52:10 +0530

    Update amazon_shipment.py

--- amazon_shipment.py
+++ amazon_shipment.py
@@ -7,6 +7,7 @@ cart = [1999, 200, 500, 399, 780, 899, 499]
 for i in cart:
     if i >= 500:
         print('Dear customer free shipment is available')
+        
     else:
         print('You have to pay Rs.60 for shipment')
         
